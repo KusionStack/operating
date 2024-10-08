KusionStack/kuperator

refactor sync pods

pkg/controllers/collaset/synccontrol/scale.go
@@ -17,10 +17,20 @@ limitations under the License.
 package synccontrol
 
 import (
+	"context"
 	"sort"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/util/retry"
+	appsv1alpha1 "kusionstack.io/kube-api/apps/v1alpha1"
 
 	collasetutils "kusionstack.io/operating/pkg/controllers/collaset/utils"
+	"kusionstack.io/operating/pkg/controllers/utils/expectations"
 	"kusionstack.io/operating/pkg/controllers/utils/podopslifecycle"
+	"kusionstack.io/operating/pkg/features"
+	"kusionstack.io/operating/pkg/utils/feature"
 )
 
 func getPodsToDelete(filteredPods []*collasetutils.PodWrapper, replaceMapping map[string]*collasetutils.PodWrapper, diff int) []*collasetutils.PodWrapper {
@@ -40,12 +50,12 @@ func getPodsToDelete(filteredPods []*collasetutils.PodWrapper, replaceMapping ma
 	return needDeletePods
 }
 
-// when sort pods to choose delete, only sort pods without replacement or these replace origin pods
+// when sort pods to choose delete, only sort (1) replace origin pods, (2) non-exclude pods
 func getTargetsDeletePods(filteredPods []*collasetutils.PodWrapper, replaceMapping map[string]*collasetutils.PodWrapper) []*collasetutils.PodWrapper {
 	targetPods := make([]*collasetutils.PodWrapper, len(replaceMapping))
 	index := 0
 	for _, pod := range filteredPods {
-		if _, exist := replaceMapping[pod.Name]; exist {
+		if _, exist := replaceMapping[pod.Name]; exist && !pod.ToExclude {
 			targetPods[index] = pod
 			index++
 		}
@@ -76,4 +86,78 @@ func (s ActivePodsForDeletion) Less(i, j int) bool {
 	}
 
 	return collasetutils.ComparePod(l.Pod, r.Pod)
+}
+
+func (r *RealSyncControl) reclaimScaleStrategy(ctx context.Context, toDeletePodNames sets.String, toExcludePodNames sets.String, toIncludedPodNames sets.String, cls *appsv1alpha1.CollaSet) error {
+	// ReclaimPodScaleStrategy FeatureGate defaults to true
+	// Add '--feature-gates=ReclaimPodScaleStrategy=false' to container args, to disable reclaim of podToDelete
+	if feature.DefaultFeatureGate.Enabled(features.ReclaimPodScaleStrategy) && len(toDeletePodNames) > 0 {
+		var newPodToDelete []string
+		var newPodToExclude []string
+		var newPodToInclude []string
+
+		for _, podName := range cls.Spec.ScaleStrategy.PodToDelete {
+			if !toDeletePodNames.Has(podName) {
+				newPodToDelete = append(newPodToDelete, podName)
+			}
+		}
+		for podName := range toExcludePodNames {
+			newPodToExclude = append(newPodToExclude, podName)
+
+		}
+		for podName := range toIncludedPodNames {
+			newPodToInclude = append(newPodToInclude, podName)
+		}
+		// update cls.spec.scaleStrategy
+		cls.Spec.ScaleStrategy.PodToDelete = newPodToDelete
+		cls.Spec.ScaleStrategy.PodToExclude = newPodToExclude
+		cls.Spec.ScaleStrategy.PodToInclude = newPodToInclude
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			return r.client.Update(ctx, cls)
+		}); err != nil {
+			return err
+		}
+		return collasetutils.ActiveExpectations.ExpectUpdate(cls, expectations.CollaSet, cls.Name, cls.ResourceVersion)
+	}
+	return nil
+}
+
+func (r *RealSyncControl) dealIncludeExcludePods(cls *appsv1alpha1.CollaSet, pods []*corev1.Pod, resources *collasetutils.RelatedResources) (sets.String, sets.String, bool, error) {
+	if len(cls.Spec.ScaleStrategy.PodToExclude) == 0 && len(cls.Spec.ScaleStrategy.PodToInclude) == 0 {
+		return nil, nil, false, nil
+	}
+
+	ownedPods := sets.String{}
+	for _, pod := range pods {
+		ownedPods.Insert(pod.Name)
+	}
+
+	excludePodNames := sets.String{}
+	for _, podName := range cls.Spec.ScaleStrategy.PodToExclude {
+		if ownedPods.Has(podName) {
+			excludePodNames.Insert(podName)
+		}
+	}
+
+	includePodNames := sets.String{}
+	for _, podName := range cls.Spec.ScaleStrategy.PodToInclude {
+		includePodNames.Insert(podName)
+	}
+
+	intersection := excludePodNames.Intersection(includePodNames)
+	if len(intersection) > 0 {
+		r.recorder.Eventf(cls, corev1.EventTypeWarning, "DupExIncludedPod", "duplicated pods %s in both excluding and including sets", strings.Join(intersection.List(), ", "))
+		includePodNames.Delete(intersection.List()...)
+		excludePodNames.Delete(intersection.List()...)
+	}
+
+	if includePodNames.Len() > 0 {
+		for _, pod := range pods {
+			if includePodNames.Has(pod.Name) {
+				includePodNames.Delete(pod.Name)
+			}
+		}
+	}
+
+	return excludePodNames, includePodNames, false, nil
 }

KusionStack/operating

pkg/controllers/collaset/synccontrol/scale.go
@@ -17,12 +17,22 @@ limitations under the License.
 package synccontrol
 
 import (
+	"context"
 	"sort"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/util/retry"
+	appsv1alpha1 "kusionstack.io/kube-api/apps/v1alpha1"
 
 	appsv1alpha1 "kusionstack.io/kube-api/apps/v1alpha1"
 
 	collasetutils "kusionstack.io/kuperator/pkg/controllers/collaset/utils"
+	"kusionstack.io/kuperator/pkg/controllers/utils/expectations"
 	"kusionstack.io/kuperator/pkg/controllers/utils/podopslifecycle"
+	"kusionstack.io/kuperator/pkg/features"
+	"kusionstack.io/kuperator/pkg/utils/feature"
 )
 
 func getPodsToDelete(filteredPods []*collasetutils.PodWrapper, replaceMapping map[string]*collasetutils.PodWrapper, diff int) []*collasetutils.PodWrapper {
@@ -52,12 +62,12 @@ func getPodsToDelete(filteredPods []*collasetutils.PodWrapper, replaceMapping ma
 	return needDeletePods
 }
 
-// when sort pods to choose delete, only sort pods without replacement or these replace origin pods
+// when sort pods to choose delete, only sort (1) replace origin pods, (2) non-exclude pods
 func getTargetsDeletePods(filteredPods []*collasetutils.PodWrapper, replaceMapping map[string]*collasetutils.PodWrapper) []*collasetutils.PodWrapper {
 	targetPods := make([]*collasetutils.PodWrapper, len(replaceMapping))
 	index := 0
 	for _, pod := range filteredPods {
-		if _, exist := replaceMapping[pod.Name]; exist {
+		if _, exist := replaceMapping[pod.Name]; exist && !pod.ToExclude {
 			targetPods[index] = pod
 			index++
 		}
@@ -88,4 +98,78 @@ func (s ActivePodsForDeletion) Less(i, j int) bool {
 
 	// TODO consider service available timestamps
 	return collasetutils.ComparePod(l.Pod, r.Pod)
+}
+
+func (r *RealSyncControl) reclaimScaleStrategy(ctx context.Context, toDeletePodNames sets.String, toExcludePodNames sets.String, toIncludedPodNames sets.String, cls *appsv1alpha1.CollaSet) error {
+	// ReclaimPodScaleStrategy FeatureGate defaults to true
+	// Add '--feature-gates=ReclaimPodScaleStrategy=false' to container args, to disable reclaim of podToDelete
+	if feature.DefaultFeatureGate.Enabled(features.ReclaimPodScaleStrategy) && len(toDeletePodNames) > 0 {
+		var newPodToDelete []string
+		var newPodToExclude []string
+		var newPodToInclude []string
+
+		for _, podName := range cls.Spec.ScaleStrategy.PodToDelete {
+			if !toDeletePodNames.Has(podName) {
+				newPodToDelete = append(newPodToDelete, podName)
+			}
+		}
+		for podName := range toExcludePodNames {
+			newPodToExclude = append(newPodToExclude, podName)
+
+		}
+		for podName := range toIncludedPodNames {
+			newPodToInclude = append(newPodToInclude, podName)
+		}
+		// update cls.spec.scaleStrategy
+		cls.Spec.ScaleStrategy.PodToDelete = newPodToDelete
+		cls.Spec.ScaleStrategy.PodToExclude = newPodToExclude
+		cls.Spec.ScaleStrategy.PodToInclude = newPodToInclude
+		if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			return r.client.Update(ctx, cls)
+		}); err != nil {
+			return err
+		}
+		return collasetutils.ActiveExpectations.ExpectUpdate(cls, expectations.CollaSet, cls.Name, cls.ResourceVersion)
+	}
+	return nil
+}
+
+func (r *RealSyncControl) dealIncludeExcludePods(cls *appsv1alpha1.CollaSet, pods []*corev1.Pod, resources *collasetutils.RelatedResources) (sets.String, sets.String, bool, error) {
+	if len(cls.Spec.ScaleStrategy.PodToExclude) == 0 && len(cls.Spec.ScaleStrategy.PodToInclude) == 0 {
+		return nil, nil, false, nil
+	}
+
+	ownedPods := sets.String{}
+	for _, pod := range pods {
+		ownedPods.Insert(pod.Name)
+	}
+
+	excludePodNames := sets.String{}
+	for _, podName := range cls.Spec.ScaleStrategy.PodToExclude {
+		if ownedPods.Has(podName) {
+			excludePodNames.Insert(podName)
+		}
+	}
+
+	includePodNames := sets.String{}
+	for _, podName := range cls.Spec.ScaleStrategy.PodToInclude {
+		includePodNames.Insert(podName)
+	}
+
+	intersection := excludePodNames.Intersection(includePodNames)
+	if len(intersection) > 0 {
+		r.recorder.Eventf(cls, corev1.EventTypeWarning, "DupExIncludedPod", "duplicated pods %s in both excluding and including sets", strings.Join(intersection.List(), ", "))
+		includePodNames.Delete(intersection.List()...)
+		excludePodNames.Delete(intersection.List()...)
+	}
+
+	if includePodNames.Len() > 0 {
+		for _, pod := range pods {
+			if includePodNames.Has(pod.Name) {
+				includePodNames.Delete(pod.Name)
+			}
+		}
+	}
+
+	return excludePodNames, includePodNames, false, nil
 }